288126c7aa7ecdd96091aecf44975fe827059693
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-18 09:00:14 +0530

    created hello.js

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+    render() {
+        return(
+            <div className='f1 tc'>
+                <h1>Hello world!</h1>
+                <p>This is a paragraph!</p>
+            </div>
+                      
+        )
+        
+    }
+}
+
+export default Hello;
