b773629cff80f026848e251883721cb92270b656
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-18 19:46:07 +0530

    using state

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = (searchChange, searchFeild) => {
     return(
         <div>
             <input type='search' 
             placeholder='Search robots...' className='bg-lightest-blue b--green pa1 ma2'
+            onchange='onSearchChange'
             />
         </div>
     )
