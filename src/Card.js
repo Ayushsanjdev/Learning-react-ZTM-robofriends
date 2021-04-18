25606cb7694a04e01e5cda51d219a4634fb06e70
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-18 11:18:37 +0530

    added some tachyon

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>
+        <div className= 'bg-light-blue br3 pd2 mr3 dib grow bw2 shadow-5'>
             <img src='https://robohash.org/cool?200x200' alt='robots' />
             <h2>Jane Doe</h2>
             <p>jane.doe@gmail.com</p>
