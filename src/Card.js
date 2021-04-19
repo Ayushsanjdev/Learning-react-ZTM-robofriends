1a1a56e8a39078726f22bfdb7ab73d0298a0ad3e
Ayush Sanj <ayushsanjpro@gmail.com>
2021-04-19 12:41:17 +0530

    added background clr

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({name, email, username, id}) => {
     return (
-        <div className= 'bg-light-yellow br3 pa3 ma2 dib grow bw2 shadow-5 tc'>
+        <div className= 'bg-light-grey br3 pa3 ma2 dib grow bw2 shadow-5 tc'>
             <img src={`https://robohash.org/${id}?size=200x200`} alt='robots' />
             <h2>{name}</h2>
             <p>{username}</p>
